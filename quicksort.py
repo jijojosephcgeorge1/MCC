bb4057be8c97dded0c2878a3ea3105f6cb9955b9
VidyaShree-MG <78849444+VidyaShree-MG@users.noreply.github.com>
2021-02-18 08:14:37 +0530

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,48 +1,35 @@
-# Python program for Quicksort
-# This function takes last element as pivot, places 
-# the pivot element at its correct position in sorted 
-# array, and places all smaller (smaller than pivot) 
-# to left of pivot and all greater elements to right 
-# of pivot 
-def partition(arr,low,high): 
-	i = ( low-1 )		 # index of smaller element 
-	pivot = arr[high]	 # pivot 
-
-	for j in range(low , high): 
-
-		# If current element is smaller than or 
-		# equal to pivot 
-		if arr[j] <= pivot: 
-		
-			# increment index of smaller element 
-			i = i+1
-			arr[i],arr[j] = arr[j],arr[i] 
-
-	arr[i+1],arr[high] = arr[high],arr[i+1] 
-	return ( i+1 ) 
-
-# The main function that implements QuickSort 
-# arr[] --> Array to be sorted, 
-# low --> Starting index, 
-# high --> Ending index 
-
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-	if low < high: 
-
-		# pi is partitioning index, arr[p] is now 
-		# at right place 
-		pi = partition(arr,low,high) 
-
-		# Separately sort elements before 
-		# partition and after partition 
-		quickSort(arr, low, pi-1) 
-		quickSort(arr, pi+1, high) 
-
-# Driver code to test above 
-arr = [10, 7, 8, 9, 1, 5] 
-n = len(arr) 
-quickSort(arr,0,n-1) 
-print ("Sorted array is:") 
-for i in range(n): 
-	print ("%d" %arr[i]), 
+def quicksort(Mylist):
+    n=len(Mylist)
+    Rec_quicksort(Mylist, 0, n-1)
+ 
+def Rec_quicksort(Mylist, first, last):
+    if first<last:
+        pos=position(Mylist, first, last)
+        Rec_quicksort(Mylist, first, pos-1)
+        Rec_quicksort(Mylist, pos+1, last)
+ 
+def position(Mylist, first, last):
+    pivot=Mylist[first]
+    i=first
+    j=last
+ 
+    while i<j:
+        while i<=j and Mylist[i]<=pivot:
+            i=i+1
+        while pivot<Mylist[j]:
+            j=j-1
+        if i<j:
+            temp=Mylist[i]
+            Mylist[i]=Mylist[j]
+            Mylist[j]=temp
+ 
+    temp=Mylist[first]
+    Mylist[first]=Mylist[j]
+    Mylist[j]=temp
+    return j
+ 
+Mylist=[ 50, 30, 10, 90, 80, 20, 40, 60 ]
+print("Given list:",Mylist)
+ 
+quicksort(Mylist)
+print("After sorting list is:",Mylist)
